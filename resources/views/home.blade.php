cad2f6de191c042e7d22339de5f650dedcecd4ba
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-19 12:14:38 +0200

    Add CSS styling and integrate with JavaScript for improved UI
    - Added app.css for custom styles including font, colors, and layout.
    - Updated app.js to import the new CSS file.
    - Modified home.blade.php to include the CSS and ensure proper rendering of the table.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Trains</title>
+    {{-- CSS --}}
+    @vite(['resources/js/app.js'])
 </head>
 <body>
     <nav>
@@ -12,7 +14,7 @@
     </nav>
 
     <main>
-        <table style="width:100%">
+        <table>
             <thead>
                 <tr>
                     <th>Codice Treno</th>
