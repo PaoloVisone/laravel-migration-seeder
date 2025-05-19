4a240f7c908508fd6d7e82ba49116cfa2f7e9921
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-19 11:50:52 +0200

    Refactor homepage route to use PageController for better organization

---
+++ resources/views/home.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trains</title>
+</head>
+<body>
+    <nav>
+        <h1>Treni</h1>
+    </nav>
+
+    <main>
+        <table style="width:100%">
+            <thead>
+                <tr>
+                    <th>Codice Treno</th>
+                    <th>Azienda</th>
+                    <th>Stazione di Partenza</th>
+                    <th>Stazione di Arrivo</th>
+                    <th>Partenza</th>
+                    <th>Arrivo</th>
+                    <th>Totale Carrozze</th>
+                    <th>Cancellato</th>
+                    <th>In Orario</th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    @foreach ($trains as $train)
+                    <tr>
+                        <td>{{ $train->codice_treno }}</td>
+                        <td>{{ $train->azienda }}</td>
+                        <td>{{ $train->stazione_di_partenza }}</td>
+                        <td>{{ $train->stazione_di_arrivo }}</td>
+                        <td>{{ $train->orario_di_partenza }}</td>
+                        <td>{{ $train->orario_di_arrivo }}</td>
+                        <td>{{ $train->totale_carrozze }}</td>
+                        <td>{{ $train->cancellato ? 'Cancellato' : 'Attivo' }}</td>
+                        <td>{{ $train->in_orario ? 'In orario' : 'In ritardo' }}</td>
+                    </tr>
+                    @endforeach
+                </tr>
+            </tbody>
+        </table>
+    </main>
+</body>
+</html>
